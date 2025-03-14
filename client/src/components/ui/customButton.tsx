c1a5cf76dc2fdd29ff0866c97d329b1eb66307b3
bishnudev35 <bishnudevsardar35@gmail.com>
2025-03-15 04:34:26 +0530

    feat: update button labels and improve New Project functionality in dashboard

--- client/src/components/ui/customButton.tsx
+++ client/src/components/ui/customButton.tsx
@@ -1,37 +1,33 @@
-import * as React from "react"
-import { Plus } from "lucide-react"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { Plus } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-const CustomButton =({
+const CustomButton = ({
     children,
     className,
     onClick,
-    type,
-}:{
-    children:React.ReactNode
-    className?:string
-    onClick?:()=>void
-    type?:"button"|"submit"|"reset"
-
-
-})=>{
+    type
+}: {
+    children: React.ReactNode;
+    className?: string;
+    onClick?: () => void;
+    type?: "button" | "submit" | "reset";
+}) => {
     return (
         <button
-        onClick={onClick}
-        type={type ?? "button"}
-        className={cn(
-          className,
-          "gradient-button-bg p-[1px] inline-flex group rounded-md text-sm font-medium focus-visible:ring-offset-1 focus-visible:ring-offset-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-        )}
-      >
-        <div className="rounded-[6px] w-full gradient-button flex items-center justify-center whitespace-nowrap px-4 py-2 h-9">
-    
-          <Plus className="w-4 h-4 mr-2" />
-          New Project
-        </div>
-      </button>
+            onClick={onClick}
+            type={type ?? "button"}
+            className={cn(
+                className,
+                "gradient-button-bg p-[1px] inline-flex group rounded-md text-sm font-medium focus-visible:ring-offset-1 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            )}
+        >
+            <div className="rounded-[6px] w-full gradient-button flex items-center justify-center whitespace-nowrap px-4 py-2 h-9">
+                <Plus className="w-4 h-4 mr-2" />
+                {children}
+            </div>
+        </button>
     );
-}
-
+};
 
 export default CustomButton;
